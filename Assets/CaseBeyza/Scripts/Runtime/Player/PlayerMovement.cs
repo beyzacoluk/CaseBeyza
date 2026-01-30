136bc90b5e23577c1949411e78d90126ca00200d
Beyza Çoluk <colukbeyza@gmail.com>
2026-01-30 15:41:23 +0300

    Player Moves

---
+++ Assets/CaseBeyza/Scripts/Runtime/Player/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 6f;
+    public bool isGrounded;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal"); // a d
+        float moveZ = Input.GetAxis("Vertical");   // w s
+
+        Vector3 velocity = rb.velocity;
+        velocity.x = moveX * moveSpeed;
+        velocity.z = moveZ * moveSpeed;
+        rb.velocity = velocity;
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
